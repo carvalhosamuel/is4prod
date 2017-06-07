d8b29fd9250fe3f07fedebb61ee97145ab8af340
carvalhosamuel <samfdm@gmail.com>
2017-06-07 15:54:22 +0100

    Blank Shinydashboard

--- server.R
+++ server.R
@@ -1,9 +1,8 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+## server.R ##
+##
+## Samuel Carvalho
+##
+## IS4PROD App Server
 
 library(shiny)
 
--- ui.R
+++ ui.R
@@ -1,31 +1,13 @@
+## ui.R ##
+##
+## Samuel Carvalho
+##
+## IS4PROD App UI
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-#07/06/2017
+library(shinydashboard)
 
-library(shiny)
-
-shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-))
+dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
